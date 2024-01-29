877614f6c3e421d930f9e6825e25afddbb706cbc
Alex Kuznietsov <o.kuznietsov@inc4.net>
2024-01-29 13:23:08 +0000

    Initial contract

---
+++ contracts/Burn.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+contract Lock {
+    uint public totalBurned = 0;
+
+    event Burned(address indexed from, uint amount);
+
+    function burn() external payable {
+        totalBurned += msg.value;
+        address payable zero = payable(address(0));
+        zero.transfer(msg.value);
+        emit Burned(msg.sender, msg.value);
+    }
+
+    function getTotal() external view returns (uint) {
+        return totalBurned;
+    }
+}
